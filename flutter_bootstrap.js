4ad476810b5f3118027c6b185f3ee3c0ce1ec0f7
mincheol-shin <mincheol-shin@naver.com>
2024-07-08 17:38:33 +0900

    test:

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4209590789"
+    serviceWorkerVersion: "1415887553"
   }
 });
